d00dc5e445c33256c3dbc071aca43381d7a8d9cc
hkarargi <hussein@ahkfastrack.com>
2023-04-10 20:54:39 +0200

    Edited Mod Helper Data Description

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,7 +5,7 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Upgradable Shooty Turret";
 
-    public const string Description = "This makes it so you can place a shooty turret that is upgradable.";
+    public const string Description = "This makes it so you can place a shooty turret that is upgradable. I've had alot of fun making this to be honest!";
 
     public const string RepoOwner = "hkarargi";
     public const string RepoName = "UpgradableShootyTurret";
