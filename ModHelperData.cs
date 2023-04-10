1ee534cca44a9fe5f1c40d71fc7a800e094833ab
hkarargi <hussein@ahkfastrack.com>
2023-04-10 20:48:40 +0200

    Removed all Images that are not mine and are unused.

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -5,8 +5,8 @@ public static class ModHelperData
     public const string Version = "1.0.0";
     public const string Name = "Upgradable Shooty Turret";
 
-    public const string Description = "";
+    public const string Description = "This makes it so you can place a shooty turret that is upgradable.";
 
-    public const string RepoOwner = "";
-    public const string RepoName = "";
+    public const string RepoOwner = "hkarargi";
+    public const string RepoName = "UpgradableShootyTurret";
 }
