bcf624375a6060857ce31ac1a50aa2320eef0061
hkarargi <hussein@ahkfastrack.com>
2023-04-15 04:26:06 +0200

    Edited Mod Helper Data

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -2,6 +2,7 @@ namespace UpgradableShootyTurret;
 
 public static class ModHelperData
 {
+    public const string WorksOnVersion = "36.0";
     public const string Version = "0.9.1";
     public const string Name = "Upgradable Shooty Turret";
 
