d5e420b003c424e24168a32034802ae50846653a
hkarargi <hussein@ahkfastrack.com>
2023-04-15 04:33:31 +0200

    update to modhelperdata again

--- ModHelperData.cs
+++ ModHelperData.cs
@@ -3,7 +3,7 @@ namespace UpgradableShootyTurret;
 public static class ModHelperData
 {
     public const string WorksOnVersion = "36.0";
-    public const string Version = "0.9.1";
+    public const string Version = "1.0.0";
     public const string Name = "Upgradable Shooty Turret";
 
     public const string Description = "This makes it so you can place a shooty turret that is upgradable. I've had alot of fun making this to be honest!";
